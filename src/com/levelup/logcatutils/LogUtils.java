cfdee630a98bdde30ace2cb0c26d6d744b255505
Steve Lhomme <robux4@gmail.com>
2012-08-07 15:04:48 +0200

    filter by the app PID and possibly by an extra tag

--- src/com/levelup/logcatutils/LogUtils.java
+++ src/com/levelup/logcatutils/LogUtils.java
@@ -11,19 +11,32 @@ public class LogUtils {
 		void finishedReadingLogs();
 	}
 
-	public static void generateLog(final LogHandler handler) {
+	public static void generateLog(final LogHandler handler, final String[] extraStrings) {
 		if (handler!=null)
 			new Thread() {
 			@Override
 			public void run() {
 				try {
-					Process process = Runtime.getRuntime().exec("logcat -d");
+					Process process = Runtime.getRuntime().exec("logcat -d -v time");
 					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
+					final String pidTest = String.format("(% 5d)", android.os.Process.myPid());
 					String line;
-					while ((line = bufferedReader.readLine()) != null)
-						handler.addNewLogLine(line);
-					
+					while ((line = bufferedReader.readLine()) != null) {
+						boolean add = false;
+						if (line.contains(pidTest))
+							add = true;
+						else if (extraStrings!=null) {
+							for (int i=0;i<extraStrings.length; ++i)
+								if (line.contains(extraStrings[i])) {
+									add = true;
+									break;
+								}
+						}
+						if (add)
+							handler.addNewLogLine(line);
+					}
+
 					handler.finishedReadingLogs();
 
 				} catch (IOException e) {
