5cc6c125b90c52e98b0dbf5d1b63be0ad23eb786
Steve Lhomme <robux4@gmail.com>
2012-08-08 14:44:18 +0200

    Add the license and a bit of documentation

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ LogcatUtils
 ===========
 
 Android library helping the users to send logs to the developers
+
+Simply call LogUtils.generateLog() with a proper handler. A file and email
+sender example of handlers are provided.
+
+License
+=======
+
+    Copyright 2012 LevelUp Studio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/com/levelup/logcatutils/LogUtils.java
+++ src/com/levelup/logcatutils/LogUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 LevelUp Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.levelup.logcatutils;
 
 import java.io.BufferedReader;
@@ -11,6 +27,11 @@ public class LogUtils {
 		void finishedReadingLogs();
 	}
 
+	/**
+	 * 
+	 * @param handler the log data receiver
+	 * @param extraStrings some additional strings you want to filter in (not based on PID)
+	 */
 	public static void generateLog(final LogHandler handler, final String[] extraStrings) {
 		if (handler!=null)
 			new Thread() {
